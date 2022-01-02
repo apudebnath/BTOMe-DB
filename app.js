66ac7a3eb427d715497bb648766589b13ddc5fed
sheikh ariful <sheikharifulislam770@gmail.com>
2022-01-03 04:36:59 +0600

    add delete api and update mongodb string search

--- app.js
+++ app.js
@@ -21,11 +21,9 @@ async function run() {
 
         //All Get Api
         
-        app.get('/', async (req, res) => {
+        app.get('/', async (req, res) => { 
             res.send("Well Come") 
         });
-        
-        
 
         app.get('/all-products', async (req, res) => {
            const {category} = req.query;
@@ -35,7 +33,7 @@ async function run() {
            let products;
 
             if(category) {
-                products = await allProduct.find({category: category}).toArray();
+                products = await allProduct.find({productCategory: {$regex: ".*" + category + ".*", $options: 'ims'}}).toArray();
                 if(products.length >= 1) {
                     res.send(products);
                 }
@@ -44,16 +42,21 @@ async function run() {
                 }
            }
            else if(productId) {
-              products = await allProduct.findOne({_id: objectId(productId)});
-              if(products === null) {
-                  res.status(404).json('Product Not Found');
+              if(productId.length === 24 ) {
+                    products = await allProduct.findOne({_id: objectId(productId)});
+                    if(products === null) {
+                        res.status(404).json('Product Not Found');
+                    }
+                    else {
+                        res.send(products);
+                    }
               }
               else {
-                  res.send(products);
+                  res.status(400).json("Please Provide A Valid Id");
               }
            }
            else if(productName) {
-               products = await allProduct.find({productName: {$regex: productName.toLowerCase()}}).toArray();
+               products = await allProduct.find({productName: {$regex: ".*" + productName + ".*", $options: 'ims'}}).toArray();
                if(products.length >= 1) {
                    res.send(products);
                }
@@ -77,20 +80,30 @@ async function run() {
         //All Post Api
 
         app.post('/create-new-user', async (req, res) => {
-            const user = {
-                name: "Sheikh Ariful Islam",
-                age: 17,
-            }
-
+            const user = req.body;
             const result = await allUsers.insertOne(user);
             res.send(result);
         });
 
-        app.post('/add-product', async (req, res) => {
-           
-
-            res.end();
+        app.post('/add-product', async (req, res) => {           
+            const product = req.body;
+            const result = await allProduct.insertOne(product);
+            res.send(result);           
             
+        })
+
+        //All Delete Request
+
+        app.delete('/delete-single-product', async (req, res) => {
+            const {prductId} = req.query;
+            if(prductId && prductId.length === 24) {
+                const result = await allProduct.deleteOne({_id: prductId});
+                res.send(result);
+                
+            }
+            else {
+                res.status(400).json('Please Provide A Valid Id');
+            }
         })
     }
     catch(error) {
